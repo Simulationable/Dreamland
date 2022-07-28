a97ad3cbacb99982a33adb15b36366fb4f6e0144
CSIGROUPS\Thanakan <thanakan@itpc.co.th>
2022-07-28 15:59:15 +0700

    Restucture to MVC

---
+++ Dreamland.Web/Controllers/OurKingdomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dreamland.Web.Controllers
+{
+    public class OurKingdomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
